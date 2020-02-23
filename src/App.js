e82e756397fefd4a814a1d0d7aeec94375f7dabf
Anas <muhd.anas@outlook.com>
2020-02-23 16:33:24 +0800

    Changed to class based -- working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-class App extends Comment {
+class App extends Component {
   render () {
     return(
       <div className="App">
