1a7a1d024d9f4c8bb32bbbaff410ad3b497503d4
Anas <muhd.anas@outlook.com>
2020-02-23 16:31:30 +0800

    Changed to class based -- not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Comment {
+  render () {
+    return(
+      <div className="App">
       <h1> Hello World</h1>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
