f6302b75beaf7865d40085db1595f642eeea74e9
Anas <muhd.anas@outlook.com>
2020-02-23 17:13:15 +0800

    JSX and Conditionals examples

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 class App extends Component {
   render () {
+    const name = 'Muhammad Anas';
+    const foo = () => 'Welcome to React!';
+    const loading = true;
+    const testing = true;
+
     return(
       <div className="App">
-      <h1> Hello World</h1>
+      <h1> Hello {name}</h1>
+      <h2> {foo()} </h2>
+      {loading ? <h4>Using a conditionals! It is true</h4> : <h4>Using a conditionals! It is false</h4>  }
+      {testing && <h4>Second type of conditional!</h4>}
       </div>
     );
   }
