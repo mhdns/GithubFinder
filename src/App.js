b73bf01753a202c3cba65bea9f350c0f9bd15003
Anas <muhd.anas@outlook.com>
2020-02-25 14:26:37 +0800

    Created UserItem component added some state within the component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {Component} from 'react';
-import Navbar from './components/layout/Navbar'
+import React, {Component, Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render () {
     return(
-      <div className="navbar bg-primary">
-        <Navbar title='Github Finder' icon='fab fa-github' />
-      </div>
+      <Fragment>
+        <div className="navbar bg-primary">
+          <Navbar title='Github Finder' icon='fab fa-github' />
+        </div>
+        <UserItem />
+      </Fragment>
     );
   }
 }
