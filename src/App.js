47da592c45699675afe33aef6e53dd5c1a2773f6
Anas <muhd.anas@outlook.com>
2020-02-25 13:36:58 +0800

    Added components folder and layput subfolder to creat the Nvabar component. Imported Component into App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render () {
-    const name = 'Muhammad Anas';
-    const foo = () => 'Welcome to React!';
-    const loading = true;
-    const testing = true;
-
     return(
       <div className="App">
-      <h1> Hello {name}</h1>
-      <h2> {foo()} </h2>
-      {loading ? <h4>Using a conditionals! It is true</h4> : <h4>Using a conditionals! It is false</h4>  }
-      {testing && <h4>Second type of conditional!</h4>}
+        <Navbar />
       </div>
     );
   }
