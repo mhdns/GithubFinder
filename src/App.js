fd44de59c4d6cf1f0cf8222b2a952c33fb2dfbf7
Anas <muhd.anas@outlook.com>
2020-02-26 21:17:21 +0800

    Removed unsused component caught by eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
