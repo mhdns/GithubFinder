d7c0b558b297f19816bde03c273cd130ac7b43db
Anas <muhd.anas@outlook.com>
2020-02-29 19:50:34 +0800

    Created links in the navbar using Link from react-router-dom.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 const Navbar = props => {
     return (
@@ -7,6 +8,14 @@ const Navbar = props => {
             <h1>
                 <i className={props.icon} />{props.title}
             </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link> {/* Using Link instead of 'a' tag preserves the state so that the searched items won't change if you come back to the page */}
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
         </nav>
     );
 };
