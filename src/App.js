1e0614449020e274fe0322426767c76f394b1f7d
Anas <muhd.anas@outlook.com>
2020-02-29 21:06:45 +0800

    Added getUser method to get user information from github API and added User component to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment, Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,6 +12,7 @@ import axios from 'axios';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -40,6 +42,16 @@ class App extends Component {
     this.setState({users:[], loading:false})
   };
 
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=\
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=\
+    ${process.env.REACT_APP_GITHUB_CLIENT_SEC}`);
+    
+    this.setState({ user:res.data, loading:false });
+  };
+
   setAlert = (msg, level) => {
       this.setState({alert: {msg, level}})
   };
@@ -61,6 +73,9 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={this.state.user} loading={this.state.loading} />
+              )}/>
             </Switch>
           </div>
         </div>
